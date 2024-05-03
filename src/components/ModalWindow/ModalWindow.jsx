e1802257f618a767a1c9f1d3560ffe2c5efdb826
MyroslavaRogoza <miroslavarogozayyy@gmail.com>
2024-05-03 11:27:15 +0300

    modal window

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -1,27 +1,46 @@
 import Modal from "react-modal";
-import css from './ModalWindow.module.css'
+import { Button, ConfigProvider } from "antd";
+import css from "./ModalWindow.module.css";
 const ModalWindow = ({ onDeleteContact, closeModal, modalIsOpen, contact }) => {
   return (
     <Modal
       isOpen={modalIsOpen}
       onRequestClose={closeModal}
       ariaHideApp={false}
-       className={css.contentStyle}
+      className={css.contentStyle}
       style={{
         overlay: {
           backgroundColor: "rgba(35, 31, 31, 0.8)",
         },
       }}
     >
-      <div>
-        <p>You definitely want to delete that contact?</p>
-        <button type="button" onClick={() => onDeleteContact(contact.id)}>
+      <div className={css.modacontantContainer}>
+        <p className={css.modalText}>
+          You definitely want to delete contact <span className={css.contactNameAccent}>{contact.name}</span>?        
+        </p>
+        <div className={css.modalBtnContainer}>
+      <Button
+            type="primary"
+            htmlType="button"
+            onClick={() => onDeleteContact(contact.id)}
+            size="large"
+            danger
+          >
           Yes
-        </button>
-        <button type="button" onClick={closeModal}>
-          No
-        </button>
-      </div>
+          </Button>
+          <ConfigProvider
+            theme={{
+              token: {
+                colorPrimary: "#91FF36",
+              },
+            }}
+          >
+            <Button type="primary" htmlType="button" onClick={closeModal} size="large">
+           No
+            </Button>
+          </ConfigProvider>
+          </div>
+          </div>
     </Modal>
   );
 };
